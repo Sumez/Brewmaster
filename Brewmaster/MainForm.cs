30f1dd432211a1d7c7972281c0bc77ee06156d01
Sumez <morten@eternal.dk>
2020-06-17 11:18:38 +0200

    Handle invalid ROMs

--- Brewmaster/MainForm.cs
+++ Brewmaster/MainForm.cs
@@ -1391,7 +1391,7 @@ private void File_OpenProjectMenuItem_Click(object sender, EventArgs e)
 		        if (currentFocusLine == null) return;
 		        Mesen.LoadCartridgeAtSameState(CurrentProject.Directory.FullName, CurrentProject.Directory.FullName + @"\" + CurrentProject.CurrentConfiguration.Filename,
 			        i => currentFocusLine.CpuAddress ?? i);
-	        }, null);
+	        }, null).ContinueWith(HandleEmulatorThreadException);
 		}
 
 		private void Run_RunApplicationMenuItem_Click(object sender, EventArgs e)
@@ -1427,7 +1427,12 @@ private void File_OpenProjectMenuItem_Click(object sender, EventArgs e)
 					return;
 				}
 				Mesen.LoadCartridge(CurrentProject.Directory.FullName, romFile);
-			}, null);
+			}, null).ContinueWith(HandleEmulatorThreadException);
+		}
+
+		private void HandleEmulatorThreadException(Task task)
+		{
+			if (task.IsFaulted && task.Exception != null) Error("Error executing emulator:\n\n" + task.Exception.InnerException.Message);
 		}
 
 
@@ -1437,7 +1442,7 @@ private void File_OpenProjectMenuItem_Click(object sender, EventArgs e)
 			{
 				if (t.Status != TaskStatus.RanToCompletion || !t.Result) return;
 				Mesen.LoadCartridge(CurrentProject.Directory.FullName, CurrentProject.Directory.FullName + @"\" + CurrentProject.CurrentConfiguration.Filename);
-			}, null);
+			}, null).ContinueWith(HandleEmulatorThreadException);
 		}
 	    private void continueWithNewBuildToolStripMenuItem_Click(object sender, EventArgs e)
 	    {
@@ -1449,8 +1454,8 @@ private void File_OpenProjectMenuItem_Click(object sender, EventArgs e)
 			    if (currentFocusLine == null) return;
 				Mesen.LoadCartridgeAtSameState(CurrentProject.Directory.FullName, CurrentProject.Directory.FullName + @"\" + CurrentProject.CurrentConfiguration.Filename, 
 				    i => currentFocusLine.CpuAddress ?? i);
-		    }, null);
-	    }
+		    }, null).ContinueWith(HandleEmulatorThreadException);
+		}
 
 		private void Run_RunAppletMenuItem_Click(object sender, EventArgs e)
         {
