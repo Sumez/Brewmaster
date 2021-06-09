d4dd725b328e202082de55b686ffe753fb065943
Sumez <morten@eternal.dk>
2021-06-09 11:08:57 +0200

    Disting file for "pipeline option"

--- Brewmaster/Pipeline/DataPipeline.cs
+++ Brewmaster/Pipeline/DataPipeline.cs
@@ -4,44 +4,6 @@ using Brewmaster.ProjectModel;
 
 namespace Brewmaster.Pipeline
 {
-	public abstract class PipelineOption
-	{
-		public abstract string TypeName { get; }
-		public override string ToString() { return Program.Language.Get(TypeName); }
-		public abstract void Process(PipelineSettings dataPipelineSettings);
-		public abstract PipelineSettings Create(AsmProjectFile file);
-		public abstract PipelineSettings Load(AsmProject project, PipelineHeader pipelineHeader);
-		public virtual PipelineHeader Save(PipelineSettings settings)
-		{
-			var header = new PipelineHeader
-			{
-				Type = TypeName,
-				LastProcessed = settings.LastProcessed,
-				Output = settings.OutputFiles.ToArray()
-			};
-			GetSettings(settings, header.Settings);
-			return header;
-		}
-		public virtual PipelineSettings Clone(PipelineSettings pipelineSettings)
-		{
-			var header = Save(pipelineSettings);
-			var newPipeline = Load(pipelineSettings.File.Project, header);
-			newPipeline.File = pipelineSettings.File;
-			newPipeline.SettingChanged();
-			return newPipeline;
-		}
-
-		public virtual void GetSettings(PipelineSettings settings, ProjectModel.Properties headerSettings)
-		{
-
-		}
-
-		public virtual void SetSettings(PipelineSettings settings, ProjectModel.Properties headerSettings)
-		{
-			
-		}
-	}
-
 	public class PipelineSettings
 	{
 		public static readonly List<PipelineOption> PipelineOptions = new List<PipelineOption>
